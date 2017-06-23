a79294e5f61eec9f8a8da9105c82156ce24bf1ac
Kairbay Muldash <kmuldash@wooppay.com>
2017-06-23 16:30:38 +0600

    Ваще норм

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -14,42 +14,37 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-8">
-                <h2>Заебись!</h2>
-
                     <?php
                     use giovdk21\yii2SyntaxHighlighter\SyntaxHighlighter as SyntaxHighlighter;
                     SyntaxHighlighter::begin(['brushes' => ['php']]);
                     echo SyntaxHighlighter::getBlock('
-                    
-                    GridView::widget([
-                    \'dataProvider\' => $dataProvider,
-                    \'filterModel\' => $searchModel,
-                    \'columns\' => [
-                        [\'class\' => \'yii\grid\SerialColumn\'],
-
-                        \'id\',
-                        \'sub_service_id\',
-                        [
-                            \'class\' => \yii\grid\DataColumn::className(),
-                            \'attribute\' => \'sub_service_id\',
-                            \'label\' => \'Наименование сервиса\',
-                            \'value\' => function ($model) {
-                                return SubService::getNameById($model->sub_service_id);
-                            },
+                    <?= GridView::widget([
+                        \'dataProvider\' => $dataProvider,
+                        \'filterModel\' => $searchModel,
+                        \'columns\' => [
+                            [\'class\' => \'yii\grid\SerialColumn\'],
+                            \'id\',
+                            \'sub_service_id\',
+                            [
+                                \'class\' => \yii\grid\DataColumn::className(),
+                                \'attribute\' => \'sub_service_id\',
+                                \'label\' => \'Наименование сервиса\',
+                                \'value\' => function ($model) {
+                                    return SubService::getNameById($model->sub_service_id);
+                                },
+                            ],
+                            \'price_aston\',
+                            \'price_rub\',
+                            \'price_kzt\',
+                            \'price_aed\',
+                            \'price_sum\',
+                            \'price_usd\',
+                            \'status\',
+    
+                            [\'class\' => \'yii\grid\ActionColumn\'],
                         ],
-                        \'price_aston\',
-                        \'price_rub\',
-                        \'price_kzt\',
-                        \'price_aed\',
-                        \'price_sum\',
-                        \'price_usd\',
-                        \'status\',
-
-                        [\'class\' => \'yii\grid\ActionColumn\'],
-                    ],
-                ])
-                    
-                    
+                    ]);
+                    ?>
                     ', 'php');
                     SyntaxHighlighter::end();
                     ?>
