584ded423320ebbcaccfa9fbbd7a8786ea654de4
Kairbay Muldash <kmuldash@wooppay.com>
2017-06-23 17:05:13 +0600

    Следующее задание, сделать таблицу и выведи её код на экран!

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -7,12 +7,5 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+Думаю можно начать с html или css
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
-</div>
