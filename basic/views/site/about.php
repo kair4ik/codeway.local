df9cfbca067f3f5ed436517ac04a9efaa20aed58
Kairbay Muldash <kmuldash@wooppay.com>
2017-06-23 17:38:22 +0600

    Наконец-то , первый набросок готов!

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -14,11 +14,12 @@ $this->params['breadcrumbs'][] = $this->title;
 <br>
 <br>
 <br>
-<br>
 
 <div class="container-fluid">
     <div class="row-fluid">
+
         <div class="col-xs-12">
+            <h1><p>Первая запись</p></h1>
             <table class="table table-bordered">
                 <thead>
                 <tr>
@@ -29,39 +30,63 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <tbody>
                 <tr>
-                    <td>
+                    <td class="col-xs-4">
                         <?php
                         use giovdk21\yii2SyntaxHighlighter\SyntaxHighlighter as SyntaxHighlighter;
                         SyntaxHighlighter::begin(['brushes' => ['php']]);
                         echo SyntaxHighlighter::getBlock('
-                    <div class="container-fluid">
-                        <div class="row-fluid">
-                            <div class="col-xs-12">
-                                <table class="table table-bordered">
-                                    <thead>
-                                    <tr>
-                                        <th>Code</th>
-                                        <th>Результат</th>
-                                    </tr>
-                                    </thead>
-                    
-                                    <tbody>
-                                    <tr>
-                                        <td>Иван</td>
-                                        <td>Чмель</td>
-                                    </tr>
-                    
-                                    </tbody>
-                                </table>
+                            <div class="container-fluid">
+                                <div class="row-fluid">
+                                    <div class="col-xs-12">
+                                        <table class="table table-bordered">
+                                            <thead>
+                                            <tr>
+                                                <th>Code</th>
+                                                <th>Результат</th>
+                                            </tr>
+                                            </thead>
+                            
+                                            <tbody>
+                                            <tr>
+                                                <td>Иван</td>
+                                                <td>Чмель</td>
+                                            </tr>
+                                            </tbody>
+                                            
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
                     ', 'php');
                         SyntaxHighlighter::end();
                         ?>
 
                     </td>
-                    <td>Чмель</td>
+                    <td>
+                        <div class="container-fluid">
+                            <div class="row-fluid">
+                                <div class="col-xs-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th>Code</th>
+                                            <th>Результат</th>
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        <tr>
+                                            <td>html</td>
+                                            <td>Результат html</td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                    </td>
                 </tr>
 
                 </tbody>
