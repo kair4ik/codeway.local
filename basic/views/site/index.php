955b1eaa1c97b7b98305fcb054c96573ac83c224
Kairbay Muldash <kmuldash@wooppay.com>
2017-06-23 16:26:06 +0600

    Вывел код на странице с подствекой синтаксиса!

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -14,12 +14,45 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-8">
-                <h2>Какой то заголовок</h2>
+                <h2>Заебись!</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    <?php
+                    use giovdk21\yii2SyntaxHighlighter\SyntaxHighlighter as SyntaxHighlighter;
+                    SyntaxHighlighter::begin(['brushes' => ['php']]);
+                    echo SyntaxHighlighter::getBlock('
+                    
+                    GridView::widget([
+                    \'dataProvider\' => $dataProvider,
+                    \'filterModel\' => $searchModel,
+                    \'columns\' => [
+                        [\'class\' => \'yii\grid\SerialColumn\'],
+
+                        \'id\',
+                        \'sub_service_id\',
+                        [
+                            \'class\' => \yii\grid\DataColumn::className(),
+                            \'attribute\' => \'sub_service_id\',
+                            \'label\' => \'Наименование сервиса\',
+                            \'value\' => function ($model) {
+                                return SubService::getNameById($model->sub_service_id);
+                            },
+                        ],
+                        \'price_aston\',
+                        \'price_rub\',
+                        \'price_kzt\',
+                        \'price_aed\',
+                        \'price_sum\',
+                        \'price_usd\',
+                        \'status\',
+
+                        [\'class\' => \'yii\grid\ActionColumn\'],
+                    ],
+                ])
+                    
+                    
+                    ', 'php');
+                    SyntaxHighlighter::end();
+                    ?>
 
             </div>
 
